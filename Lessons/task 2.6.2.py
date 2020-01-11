2158f2b1ad0e2c7d4082dbe884c339d8eff0b164
illia <esculap.andreev@gmail.com>
2020-01-12 00:25:20 +0200

    add 2.6.2 file

---
+++ Lessons/task 2.6.2.py
@@ -0,0 +1,23 @@
+# Напишите программу, которая выводит часть последовательности 1 2 2 3 3 3 4 4 4 4 5 5 5 5 5 ... (число повторяется
+# столько раз, чему равно). На вход программе передаётся неотрицательное целое число n — столько элементов
+# последовательности должна отобразить программа. На выходе ожидается последовательность чисел, записанных через пробел
+# в одну строку.
+#
+# Например, если n = 7, то программа должна вывести 1 2 2 3 3 3 4.
+
+input_number = int(input())
+counter = 1
+digit = 1
+initial_string = ''
+
+while counter <= input_number:
+    initial_string += (str(digit)+' ')*digit
+    counter += digit
+    digit += 1
+
+srt_without_last_space = initial_string[:-1]
+list_of_nums = srt_without_last_space.split(' ')
+cropped_list = list_of_nums[0:input_number]
+result = ' '.join(cropped_list)
+
+print(result)
